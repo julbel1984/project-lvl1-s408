ef25242026ca8c7452875b4d8e1f9fb30f6d2bf5
julbel1984 <julbel1984@yandex.ru>
2019-01-09 21:03:48 +0300

    add brain-even and update Cli.php, composer.json

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,37 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
-function run()
+const CORRECT_ANSWER = 3;
+
+function welcome()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Welcome to the Brain Game! \n");
+    $name = prompt("May I have your name?");
+    line("Hello, {$name}!");
+}
+
+function gameLogic($gameConditionsEven, $dataAttributes)
+{
+    line("Welcome to the Brain Game! \n");
+    line($gameConditionsEven);    
+    $name = prompt("May I have your name?");    
+    line("Hello, {$name}!");   
+
+    for ($i = 1; $i <= CORRECT_ANSWER; $i += 1) {
+        [$question, $answer] = $dataAttributes();
+        
+        line("Question: {$question}");
+        $currentAnswer = prompt("Your answer");
+        
+        if ($currentAnswer === $answer) {
+            line("Correct!");
+            continue;
+        } else {
+            line("'{$currentAnswer}' is wrong answer ;(. Correct answer was '{$answer}'");
+            line("Let`s try again, {$name}!");
+            return;
+        }
+    }
+
+    return line("Congratulations, {$name}!");
 }
---
+++ src/games/even.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function \BrainGames\Cli\gameLogic;
+
+const GAME_CONDITIONS_EVEN = 'Answer "yes" if number even otherwise answer "no".';
+const RANDOM_MIN = 0;
+const RANDOM_MAX = 100;
+
+function isEven($number)
+{
+    return $number % 2 === 0 ? true : false;
+}
+
+function getAnswer($question)
+{
+    $answer = isEven($question) ? 'yes' : 'no';
+    return $answer;
+}
+
+function getRandomNumber()
+{    
+    return rand(RANDOM_MIN, RANDOM_MAX);
+}
+
+function play()
+{
+    $dataAttributes = function () {       
+        $question = getRandomNumber();
+        $answer = getAnswer($question);
+
+        return [$question, $answer];
+    };
+    
+    gameLogic(GAME_CONDITIONS_EVEN, $dataAttributes);
+    return;
+}

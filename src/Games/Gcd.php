e2ccd28d565b6214f2fa6640c90aad2ba4b9f9c9
julbel1984 <julbel1984@yandex.ru>
2019-01-11 23:30:54 +0300

    add brain-gcd

---
+++ src/Games/Gcd.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BrainGames\Games\Gcd;
+
+use function \BrainGames\GameLogic\gameLogic;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+const RANDOM_MIN = 1;
+const RANDOM_MAX = 20;
+
+function gcd($a, $b)
+{
+    if ($b === 0) {
+        return abs($a);
+    }
+    return gcd($b, $a % $b);
+}
+
+function play()
+{
+    $dataAttributes = function () {
+        $firstNumber = rand(RANDOM_MIN, RANDOM_MAX);
+        $secondNumber = rand(RANDOM_MIN, RANDOM_MAX);
+        $question = "{$firstNumber}  {$secondNumber}";
+        $answer = (string) (gcd($firstNumber, $secondNumber));
+        return [$question, $answer];
+    };
+    
+    gameLogic(DESCRIPTION, $dataAttributes);
+}

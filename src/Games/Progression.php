5219797486b5a007f324e221391547879e838c7e
julbel1984 <julbel1984@yandex.ru>
2019-01-13 17:34:00 +0300

    fix brain-progression

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -7,10 +7,10 @@ use function \BrainGames\GameLogic\gameLogic;
 const DESCRIPTION = 'What number is missing in the progression?';
 const PROGRESSION_LENGTH = 10;
 
-function generateProgression($start, $step)
+function generateProgression($start, $step, $lenProgression)
 {
     $progression = [];
-    for ($i = 0; $i < PROGRESSION_LENGTH; $i += 1) {
+    for ($i = 0; $i < $lenProgression; $i += 1) {
         $progression[] = $start + $step * $i;
     }
     return $progression;
@@ -21,7 +21,7 @@ function play()
     $generateGameData = function () {
         $start = rand(1, 10);
         $step = rand(1, 10);
-        $progression = generateProgression($start, $step);
+        $progression = generateProgression($start, $step, PROGRESSION_LENGTH);
         $progressionWithHiddenElement = array_slice($progression, 0);
         $hiddenElementIndex = rand(0, PROGRESSION_LENGTH - 1);
         $progressionWithHiddenElement[$hiddenElementIndex] = '..';
